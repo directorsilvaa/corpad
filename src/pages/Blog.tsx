bffa3a8dd1b3c4a89e95a5a1275f4ae0c82c6092
directorsilvaa <matheusgatynho2016@gmail.com>
2026-06-17 16:58:31 -0300

    Ajusta logo do blog para fundo claro

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -880,7 +880,9 @@ function BlogNavbar() {
       <a className="brand" href="/" aria-label="Pagina inicial">
         <Image
           className="brand-logo"
-          src="/logo.png"
+          src="/logo-light.png"
+          srcSet="/logo-light-180.png 180w, /logo-light-300.png 300w, /logo-light-600.png 600w, /logo-light.png 1500w"
+          sizes="(max-width: 620px) 142px, 249px"
           alt="Logo CORPAD"
           width={1500}
           height={390}
